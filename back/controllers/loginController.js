f8a1a473fd33cb5f54608d2b0b171812da4341b6
MatteoFar <matteo.farigu@gmail.com>
2023-06-23 16:13:49 +0200

    adding login crypting verification

--- back/controllers/loginController.js
+++ back/controllers/loginController.js
@@ -1,27 +1,24 @@
+import CryptoJS from "crypto-js";
 import { UserModel } from "../models/User.js";
 
 export default async function loginController(req, res) {
   try {
     const { email, password } = req.body;
 
-    // console.log("BODY", req.body);
-
     // vérifier si l'utilisateur existe
-
     const user = await UserModel.findOne({ email });
-
-    // console.log(email, password);
 
     if (user !== null) {
       // création de la session et redirection vers le dashboard
-      req.session.auth = true;
 
-      console.log(user);
-      // redirection vers la page
+      // vérification password
+      const passwordDecrypted = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.SECRET_CRYPTO_KEY
+      ).toString(CryptoJS.enc.Utf8);
 
-      // checker password
-
-      if (password === user.password) {
+      if (password === passwordDecrypted) {
+        req.session.auth = true;
         res
           .status(200)
           .send({ message: "connexion effectué !", req: req.session });
@@ -30,8 +27,6 @@ export default async function loginController(req, res) {
       res
         .status(400)
         .send({ message: "les mots de passes ne correcspondent pas !" });
-
-      console.log(req.session);
     } else {
       res.status(400).send({ message: "l'utilisateur n'existe pas !" });
       return;
